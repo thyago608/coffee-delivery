7fb52fa506447c030c6d9bd8affa02b884ca16ea
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-12-10 18:35:16 -0400

    Added React router dom and configure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from 'styles/global'
 import { defaultTheme } from 'styles/themes/default'
+import { Router } from './router'
+import { BrowserRouter } from 'react-router-dom'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,3 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>,
 )
-
-/*
-
-<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@700;800&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-
-
-*/
