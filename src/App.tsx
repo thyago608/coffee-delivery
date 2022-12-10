460c3f3c577bf3d850703fa4528321b1a520c163
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-12-09 20:04:59 -0400

    Added Fonts and Styles Global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from 'styles/global'
 import { defaultTheme } from 'styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>Coffee Delivery</div>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background: ${(props) => props.theme.colors.background};
+        color: ${(props) => props.theme.colors['base-text']};
+        -webkit-font-smoothing: antialiased;
+    }
+
+    *, body{
+        font-family: ${(props) => props.theme.fonts.roboto};
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    a{
+        text-decoration:none;
+    }
+`
